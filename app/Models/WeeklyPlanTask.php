415c0e61b7ba063cca839a88c4dbcedb56c23973
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-03 16:37:31 -0600

    feat: add logs relationship and update logged fields in WeeklyPlanTask model

--- app/Models/WeeklyPlanTask.php
+++ app/Models/WeeklyPlanTask.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 #[Fillable(['boxes', 'produced_boxes', 'pallets', 'produced_pallets', 'hours', 'weighed_pounds', 'destination', 'operation_date', 'start_date', 'end_date', 'weekly_plan_id', 'line_sku_id', 'status'])]
 class WeeklyPlanTask extends Model
 {
+    public const LOGGED_FIELDS = ['boxes', 'operation_date', 'line_sku_id'];
+
     protected $casts = [
         'operation_date' => 'datetime',
     ];
@@ -20,5 +22,10 @@ class WeeklyPlanTask extends Model
     public function weeklyPlan()
     {
         return $this->belongsTo(WeeklyPlan::class);
+    }
+
+    public function logs()
+    {
+        return $this->hasMany(WeeklyPlanTaskLog::class);
     }
 }
