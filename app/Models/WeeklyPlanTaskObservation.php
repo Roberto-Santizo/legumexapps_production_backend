d28eeca67068960126ae313c66a5592a3adf88fe
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-04 17:15:47 -0600

    feat: add observations relationship to WeeklyPlanTask and create WeeklyPlanTaskObservation model

---
+++ app/Models/WeeklyPlanTaskObservation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['weekly_plan_task_id', 'user_id', 'observation'])]
+class WeeklyPlanTaskObservation extends Model
+{
+    public function task()
+    {
+        return $this->belongsTo(WeeklyPlanTask::class, 'weekly_plan_task_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
