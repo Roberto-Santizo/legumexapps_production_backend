2bae19e25c30064c4e1baacf31e75e350899c7ca
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-04 17:21:19 -0600

    feat: create WeeklyPlanTaskObservationResource for task observation data transformation

---
+++ app/Http/Resources/WeeklyPlanTaskObservations/WeeklyPlanTaskObservationResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\WeeklyPlanTaskObservations;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WeeklyPlanTaskObservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' =>                  $this->id,
+            'weekly_plan_task_id' => $this->weekly_plan_task_id,
+            'observation' =>         $this->observation,
+            'user_id' =>             $this->user_id,
+            'user_name' =>           $this->user->name,
+            'created_at' =>          $this->created_at->format('d-m-Y H:i'),
+            'updated_at' =>          $this->updated_at->format('d-m-Y H:i'),
+            'was_edited' =>          $this->updated_at->ne($this->created_at),
+        ];
+    }
+}
